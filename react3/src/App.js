d8f0faff3c31d6042bbe30d29ae6d5f5005d69b6
Muhammad Ghufran <darazone04@gmail.com>
2024-11-03 21:40:13 +0500

    Start Game final

--- react3/src/App.js
+++ react3/src/App.js
@@ -1,18 +1,17 @@
+import StartGame from './Pages/StartGame'
+import { useState } from 'react'
 import './App.css'
-import Button from './Components/Button'
+import PlayGame from './Pages/PlayGame'
 
 const App = () => {
+  let [isStarted, setStarted] = useState(false)
+  const toggleBtn = () =>{
+    setStarted((pre)=>!pre)
+  }
   return (
-    <div className='flex justify-evenly items-center text-center h-[100vh]'>
-
-    <div className ='max-w-[40%]'>
-      <img src="/images/dices.png" alt="dices" width={"100%"}/>
-    </div>
-    <div className='flex flex-col items-end justify-center gap-10'>
-      <h1 className='text-[8rem] font-semibold'>Dice Game</h1>
-      <Button text={"Play Game"}/>
-    </div>
-    </div>
+<>
+  {isStarted?<PlayGame/>:<StartGame toggle={toggleBtn}/>}
+</>  
   )
 }
 
