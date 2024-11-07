87001071f7299d4afef3555d220983fc535e7409
Muhammad Ghufran <darazone04@gmail.com>
2024-11-07 18:01:52 +0500

    model issue fix

--- react5/src/Components/Model.jsx
+++ react5/src/Components/Model.jsx
@@ -6,15 +6,14 @@ const Model = ({ open, onClose, children }) => {
   return createPortal(
     <>
       {open && (
-        <>
-          <div className="bg-white min-h-[40rem] max-w-[50%] mx-auto p-4 mt-4 rounded-lg relative z-50">
+          <div className="grid place-items-center backdrop-blur h-screen w-screen top-0 absolute z-40" >
+          <div className="bg-white min-h-[40rem] min-w-[50%] mx-auto p-4 rounded-lg relative z-50">
             <div className="flex justify-end">
               <AiOutlineClose onClick={onClose} className="text-[3rem]" />
             </div>
             {children}
           </div>
-          <div onClick={onClose} className="backdrop-blur h-screen w-screen top-0 absolute z-40" />
-        </>
+        </div>
       )}
     </>,
     document.getElementById("model-root")
--- react5/src/Components/OutOfContacts.jsx
+++ react5/src/Components/OutOfContacts.jsx
@@ -4,7 +4,7 @@ const OutOfContacts = () => {
   return (
     <div className='flex items-center gap-10 mt-10'>
         <img src="/images/contact.png" alt="" width={'40rem'}/>
-        <h1 className='text-[3rem] text-white font-medium'>Out of Contacts</h1>
+        <h1 className='text-[3rem] text-white font-medium'>Contacts not found</h1>
     </div>
   )
 }
