dd49855ef1ad4887a3bbb88df595f0b7da1a886d
Muhammad Ghufran <darazone04@gmail.com>
2024-11-07 18:39:56 +0500

    project setup

---
+++ react_crypto_app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css"
+import Dashboard from "./Pages/Dashboard"
+const App = () => {
+  return (
+    <div>
+      <Dashboard/>
+    </div>
+  )
+}
+
+export default App
---
+++ react_crypto_app/src/Pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import SideNav from "../Components/SideNav"
+const Dashboard = () => {
+  return (
+    <div>
+      <SideNav/>
+    </div>
+  )
+}
+
+export default Dashboard
