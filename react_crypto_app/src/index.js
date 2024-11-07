a66ae2d4f9a9ccc7e57e44f41621755b8f6f56fd
Muhammad Ghufran <darazone04@gmail.com>
2024-11-07 22:49:05 +0500

    install ubunut font

--- react_crypto_app/src/index.js
+++ react_crypto_app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "@fontsource/ubuntu";
+import "@fontsource/ubuntu/400.css";
+import "@fontsource/ubuntu/500.css";
+import "@fontsource/ubuntu/700.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
