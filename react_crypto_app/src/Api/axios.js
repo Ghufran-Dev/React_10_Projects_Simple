2f8942b65556650092f8783c29cc4e46aa18b8b1
Muhammad Ghufran <darazone04@gmail.com>
2024-11-18 18:03:25 +0500

    install dotenv

--- react_crypto_app/src/Api/axios.js
+++ react_crypto_app/src/Api/axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios"
 const Ax = Axios.create({
-    // baseURL: process.env.REACT_APP_BASE_URL,
-    baseURL: "http://localhost:9000",
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    // baseURL: "http://localhost:9000",
     withCredentials:true,
 })
 
